d9ea0d2c4069ed920a414dce74ea3abe1c53da07
Mary <maryandreeva100@gmail.com>
2023-02-25 18:29:02 +0300

    Update tasks functions

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Hello')
+/*
+  Дан список задач.
+  Сделать функции:
+  Добавление задачи в конец
+  Удаление задачи по названию
+  Перенос задачи в начало списка по названию
+*/
+
+let tasks = ['Задача 1'];
+
+function addTaskToEnd (task, arr) {
+  return arr.push(task);
+};
+addTaskToEnd('Задача 2', tasks);
+console.log(tasks);
+
+function removeTaskOnName (task, arr) {
+  let taskIndex = arr.indexOf(task);
+  if (taskIndex === -1) return;
+  return arr.splice(taskIndex, 1);
+}
+removeTaskOnName('Задача 1', tasks);
+console.log(tasks);
+
+let tasks2 = ['1', '2', '3']
+function transferTasktoStart(task, arr) {
+  let taskIndex = arr.indexOf(task);
+  if (taskIndex === -1) return;
+  let transferedTask = arr.splice(taskIndex, 1)[0];
+  arr.unshift(transferedTask);
+}
+transferTasktoStart(5, tasks2);
+console.log(tasks2)
