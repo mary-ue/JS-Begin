8afc67d6fccff59dff01c9b01ce4b1644df1cbf5
Mary <maryandreeva100@gmail.com>
2023-02-28 22:48:58 +0300

    New exercise

---
+++ 15.10/app.js
@@ -0,0 +1,31 @@
+/* 
+  Дополнить объект методами для получения имени:
+    - компании,
+    - ceo,
+    - сотрудника
+*/
+
+const company = {
+  name: 'ООО Агро',
+  employees: [
+    {
+      name: 'Света',
+      getName: function() {
+        return this.name;
+      }
+    }
+  ], 
+  ceo: {
+    name: 'Вася',
+    getName: function() {
+      return this.name;
+    },
+  },
+  getName: function() {
+    return this.name;
+  }
+}
+
+console.log(company.getName());
+console.log(company.ceo.getName());
+console.log(company.employees.map(employee => employee.getName()));
